7a7cd1afba5b75a0e2014216e676377e3606bc2c
Gaurav Ruhela <gauravruhela@ip-192-168-1-115.ap-south-1.compute.internal>
2023-08-16 07:28:24 +0400

    menu dropdown with animation

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,15 @@
 import React from 'react';
 import Animation1 from "./component/Animation1"
 import Hover from './component/Hover';
+import Menu from './component/Menu';
 
 const App = () => {  
 
   return (
     <div className="">
       {/* <Animation1 /> */}
-      <Hover />
+      {/* <Hover /> */}
+      <Menu />
     </div>
   )
 }
